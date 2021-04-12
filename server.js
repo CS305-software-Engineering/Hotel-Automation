fd12963f509a46a0684337f9044d23ebe3a9dce0
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2021-04-13 02:11:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+ require('dotenv').config()
  const express = require('express')
  const app = express()
  const PORT = process.env.PORT || 3000
@@ -5,50 +6,50 @@
  const expressLayout = require('express-ejs-layouts')
  const path = require('path')
  const mongoose = require('mongoose')
- //const flash = require('express-flash')
+ const session = require('express-session')
+ const flash = require('express-flash')
+ const MongoDbStore = require('connect-mongo')
+ const passport = require('passport')
 //mongodb connection
 const url = 'mongodb+srv://harshagarg09:harshagarg09@cluster0.iizlz.mongodb.net/atithi?retryWrites=true&w=majority'
 mongoose.connect(url,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
     }
 ).then(() => {
     console.log("Database connected")
 });
- 
+const connection = mongoose.connection;
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+//Session store
+let mongoStore = MongoDbStore.create({
+                        mongoUrl: url,
+                     //   mongooseConnection: connection,
+                        collection: 'sessions'
+                    })
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24}
+}))
 //Assets
-// app.use(flash())
+ app.use(flash())
  app.use(express.static('public'))
  app.use(express.urlencoded({extended : false}))
  app.use(express.json())
  require('./routes/web')(app)
 
-
-app.get('/managerhome', (req,res)=>{
-  res.render('hotel/managerhome')
-})
-
-app.get('/viewprofile', (req,res)=>{
-  res.render('hotel/viewprofile')
-})
-
-app.get('/neworders', (req,res)=>{
-  res.render('hotel/neworders')
-})
-
-app.get('/addstaff', (req,res)=>{
-  res.render('hotel/addstaff')
-})
-
-app.get('/completedorder', (req,res)=>{
-  res.render('hotel/completedorder')
-})
-
-app.get('/rawmaterials', (req,res)=>{
-  res.render('hotel/rawmaterials')
-})
 
 app.listen(PORT, () => {
    console.log(`Listening on port ${PORT}`)
