5f57e366740be85c2ee1793f6f74ba21bb108263
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2021-04-13 01:58:25 +0530

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -16,7 +16,9 @@ function initRoutes(app){
      app.get('/u_register', authController().register_customer)
      app.post('/u_register', authController().postRegister_customer)
      app.get('/manager_register', authController().register_manager)
+     app.post('/manager_register', authController().postRegister_manager)
      app.get('/login', authController().login)
+     app.post('/login', authController().postLogin)
 
       app.get('/manager', (req,res)=>{
         res.render('hotel/manager')
