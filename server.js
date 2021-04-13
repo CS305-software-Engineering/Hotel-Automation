b78b93fb28407a6bd7b3ff153d5066d3a78fcf38
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2021-04-13 22:58:43 +0530

    Added files via upload

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+ require('dotenv').config()
  const express = require('express')
  const app = express()
  const PORT = process.env.PORT || 3000
@@ -5,21 +6,45 @@
  const expressLayout = require('express-ejs-layouts')
  const path = require('path')
  const mongoose = require('mongoose')
- //const flash = require('express-flash')
+ const session = require('express-session')
+ const flash = require('express-flash')
+ const MongoDbStore = require('connect-mongo')
+ const passport = require('passport')
 //mongodb connection
 const url = 'mongodb+srv://harshagarg09:harshagarg09@cluster0.iizlz.mongodb.net/atithi?retryWrites=true&w=majority'
 mongoose.connect(url,
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
     }
 ).then(() => {
     console.log("Database connected")
 });
- 
+const connection = mongoose.connection;
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+app.use(passport.initialize())
+app.use(passport.session())
+//Session store
+let mongoStore = MongoDbStore.create({
+                        mongoUrl: url,
+                     //   mongooseConnection: connection,
+                        collection: 'sessions'
+                    })
+//Session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24}
+}))
 //Assets
-// app.use(flash())
+ app.use(flash())
  app.use(express.static('public'))
  app.use(express.urlencoded({extended : false}))
  app.use(express.json())
