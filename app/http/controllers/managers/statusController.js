e5d96840bdfa1e972882265be14310a1013abeb8
harshagarg09 <39459950+harshagarg09@users.noreply.github.com>
2021-05-16 16:10:06 +0530

    Added order decline functionality

--- app/http/controllers/managers/statusController.js
+++ app/http/controllers/managers/statusController.js
@@ -2,15 +2,30 @@ const Order = require('../../../models/order')
 function statusController() {
     return {
         update(req,res){
-            Order.updateOne({_id: req.body.orderId}, {status: req.body.status}, (err, data) => {
-                if(err){
+            if(req.body.status == 'declined'){
+                Order.remove({_id: req.body.orderId}, (err, data) => {
+                    if(err){
+                        return res.redirect('/manager/orders')
+                    }
+                    
+                    // Emit event 
+                    const eventEmitter = req.app.get('eventEmitter')
+                    eventEmitter.emit('orderUpdated', { id: req.body.orderId, status: req.body.status })
                     return res.redirect('/manager/orders')
-                }
-                // Emit event 
-                const eventEmitter = req.app.get('eventEmitter')
-                eventEmitter.emit('orderUpdated', { id: req.body.orderId, status: req.body.status })
-                return res.redirect('/manager/orders')
-            })
+                })
+            }else{
+                Order.updateOne({_id: req.body.orderId}, {status: req.body.status}, (err, data) => {
+                    if(err){
+                        return res.redirect('/manager/orders')
+                    }
+                    
+                    // Emit event 
+                    const eventEmitter = req.app.get('eventEmitter')
+                    eventEmitter.emit('orderUpdated', { id: req.body.orderId, status: req.body.status })
+                    return res.redirect('/manager/orders')
+                })
+            }
+            
 
         }
     }
